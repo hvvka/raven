97a53645bfb1e650c68f27fad740f25c8001bc57
Mateusz Najda <mateusznajda@gmail.com>
2019-12-02 13:59:01 +0100

    Added mounting of output from producer

--- flight-producer/mount.sh
+++ flight-producer/mount.sh
@@ -3,3 +3,4 @@
 dir=$(pwd)
 
 sshfs -o allow_other -o cache=yes -o kernel_cache -o compression=no pdzd@40.118.42.162:/home/pdzd/input $dir/input
+sshfs -o allow_other -o cache=yes -o kernel_cache -o compression=no pdzd@40.118.42.162:/home/pdzd/output $dir/output
