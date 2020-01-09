92f90b4fd7bcbedfe6e328e642deaa4245b5981e
Mateusz Najda <mateusznajda@gmail.com>
2020-01-09 08:54:03 +0100

    cleanup to lessen depression

--- flight-consumer/src/test/java/pl/edu/pwr/raven/flightconsumer/MessageListenerTests.java
+++ flight-consumer/src/test/java/pl/edu/pwr/raven/flightconsumer/MessageListenerTests.java
@@ -1,33 +1,21 @@
 package pl.edu.pwr.raven.flightconsumer;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.*;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
-import org.springframework.kafka.test.utils.ContainerTestUtils;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import pl.edu.pwr.raven.flightconsumer.flight.Flight;
 import pl.edu.pwr.raven.flightconsumer.flight.FlightBuilder;
 import pl.edu.pwr.raven.flightconsumer.flight.FlightRepository;
-
-import java.util.Map;
 
 import static org.mockito.Mockito.verify;
 
@@ -47,19 +35,17 @@ public class MessageListenerTests {
 
     private static final String TOPIC_NAME = "raw-flight-data";
 
-    @Autowired
-    private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
-
     @ClassRule
     public static EmbeddedKafkaRule embeddedKafka =
             new EmbeddedKafkaRule(1, false, TOPIC_NAME);
 
-    private MessageProducer producerMock = Mockito.mock(MessageProducer.class);
+    @MockBean
+    private MessageProducer producerMock;
 
     @MockBean
-    private FlightRepository mockRepository;// = Mockito.mock(FlightRepository.class);
+    private FlightRepository mockRepository;
 
-    @InjectMocks
+    @Autowired
     private MessageListener sut;
 
     @Autowired
@@ -67,26 +53,8 @@ public class MessageListenerTests {
 
     @Before
     public void setUp() {
-        // this.producer = buildKafkaTemplate();
         this.producer.setDefaultTopic(TOPIC_NAME);
-
-//        // wait until the partitions are assigned
-//        for (MessageListenerContainer messageListenerContainer : kafkaListenerEndpointRegistry
-//                .getListenerContainers()) {
-//            ContainerTestUtils.waitForAssignment(messageListenerContainer,
-//                    embeddedKafka.getEmbeddedKafka().getPartitionsPerTopic());
-//        }
-
-        // this.sut = new MessageListener(mockRepository, producerMock);
     }
-
-//    private KafkaTemplate<String, String> buildKafkaTemplate() {
-//        Map<String, Object> senderProps = KafkaTestUtils.producerProps(embeddedKafka.getEmbeddedKafka());
-//        senderProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-//        senderProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-//        ProducerFactory<String, String> pf = new DefaultKafkaProducerFactory<>(senderProps);
-//        return new KafkaTemplate<>(pf);
-//    }
 
     @Test
     public void dummyTest() throws Exception {
